e6f6ac831a2d4ff0f1573344b945fd4a9eee4560
user.steven <steven.2010.to@gmail.com>
2012-03-19 07:29:34 -0400

    This is just for demo test1.

---
+++ TestEGit/src/assign2/EGitTest/Demo1.java
@@ -0,0 +1,13 @@
+package assign2.EGitTest;
+
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a demo test!!!  -  one");
+	}
+
+}
