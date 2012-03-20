b742627489ae55fd84e5e64bc059d0a6c5fee7cc
user.steven <steven.2010.to@gmail.com>
2012-03-20 04:25:01 -0400

    Demo2 changed

--- TestEGit/src/assign2/EGitTest/Demo2.java
+++ TestEGit/src/assign2/EGitTest/Demo2.java
@@ -7,7 +7,9 @@ public class Demo2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int i = 0;
+        System.out.println("Steven Test! " + i);
+        i++;
 	}
 
 }
